5c9141e1e2e727b9f7a811585e8cbf66dbe71fd9
Magno <workmag@outlook.com>
2020-02-22 05:48:10 -0400

    feat(database): add database configuration
    Added Sequelize ORM to manipulate the data and improve the development. Selected Postgres to work
    with this project..

---
+++ src/database/index.js
@@ -0,0 +1,27 @@
+import Sequelize from 'sequelize';
+
+import User from '../app/models/User';
+
+import databaseConfig from '../config/database';
+
+const models = [User];
+
+class Database {
+    constructor() {
+        this.init();
+    }
+
+    // Starts DB connection for each model
+    init() {
+        this.connection = new Sequelize(databaseConfig);
+
+        models
+            .map(model => model.init(this.connection))
+            .map(
+                model =>
+                    model.associate && model.associate(this.connection.models)
+            );
+    }
+}
+
+export default new Database();
