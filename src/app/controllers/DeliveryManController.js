d86776538a724ac1d62289d5dc22f3716f240311
Magno <workmag@outlook.com>
2020-03-23 17:50:16 -0300

    feat(deliveryman): add deliveryman requests
    add list, show, create, update, delete for deliveryman and permit the store of avatar_id file

---
+++ src/app/controllers/DeliveryManController.js
@@ -0,0 +1,106 @@
+import DeliveryMan from '../models/DeliveryMan';
+
+class DeliveryManController {
+    async index(req, res) {
+        const { page = 1 } = req.query;
+
+        const deliveryMen = await DeliveryMan.findAll({
+            limit: 20,
+            offset: (page - 1) * 20,
+            order: ['name'],
+            attributes: [
+                'id',
+                'name',
+                'email',
+                'avatar_id'
+            ],
+        });
+
+        return res.json(deliveryMen);
+    }
+
+    async show(req, res) {
+        const { id } = req.params;
+
+        const deliveryMan = await DeliveryMan.findByPk(id);
+
+        if(!deliveryMan) {
+            return res.status(404).json({ message: 'Delivery man not found'});
+        }
+
+        return res.json(deliveryMan);
+    }
+
+    async store(req, res) {
+        const deliveryManExists = await DeliveryMan.findOne({
+            where: {
+                email: req.body.email
+            },
+        });
+
+        if(deliveryManExists) {
+            return res.status(400).json({ message: 'DeliveryMan already exists' });
+        }
+
+        const { name, email } = req.body;
+
+        const { id } = await DeliveryMan.create({
+            name,
+            email,
+        });
+
+        if(!deliveryMan) {
+            return res.status(500).json({ message: "Could not create DeliveryMan" });
+        }
+
+        return res.status(202).json({ id, name, email });
+    }
+
+    async update(req, res) {
+        const { id } = req.params;
+        const { email } = req.body;
+
+        const deliveryMan = await DeliveryMan.findByPk(id);
+
+        if(!deliveryMan) {
+            return res.status(404).json({ message: 'Delivery man not found'});
+        }
+
+        if(email !== deliveryMan.email) {
+            const deliveryManExists = await User.findOne({
+                where: {
+                    email
+                },
+            });
+
+            if(deliveryManExists) {
+                return res.status(400).json({ error: "Delivery man already exists"});
+            }
+        }
+
+        const { name, avatar_id } = await deliveryMan.update(req.body);
+
+        return res.json({
+            id,
+            name,
+            email,
+            avatar_id
+        });
+    }
+
+    async delete(req, res) {
+        const { id } = req.params;
+
+        const deliveryMan = await DeliveryMan.findByPk(id);
+
+        if(!deliveryMan) {
+            return res.status(404).json({ message: 'Delivery man not found' });
+        }
+
+        await deliveryMan.destroy();
+
+        return res.json({ message: 'deleted succesfully' });
+    }
+}
+
+export default new DeliveryManController();
--- src/routes.js
+++ src/routes.js
@@ -7,10 +7,13 @@ import auth from './app/middlewares/auth';
 
 import UserController from './app/controllers/UserController';
 import RecipientController from './app/controllers/RecipientController';
-import DeliveryManController from './app/controllers/DeliveryMan';
+import DeliveryManController from './app/controllers/DeliveryManController';
+import FileController from './app/controllers/FileController';
 
 import validateSessionStore from './app/validators/SessionStore';
 import validateRecipientStore from './app/validators/RecipientStore';
+import validateDeliveryManStore from './app/validators/DeliveryManStore';
+import validateDeliveryManUpdate from './app/validators/DeliveryManUpdate';
 
 const routes = new Router();
 const upload = new multer(multerConfig);
@@ -20,6 +23,13 @@ routes.post('/login', validateSessionStore, UserController.store);
 routes.use(auth);
 
 routes.post('/recipients', validateRecipientStore, RecipientController.store);
-routes.post('/delivery-man', upload.single('avatar_id'), DeliveryManController.store);
+
+routes.get('/delivery-man', DeliveryManController.index);
+routes.get('/delivery-man/:id', DeliveryManController.show)
+routes.post('/delivery-man', validateDeliveryManStore, DeliveryManController.store);
+routes.put('/delivery-man/:id', validateDeliveryManUpdate, DeliveryManController.update);
+routes.delete('/delivery-man/:id', DeliveryManController.delete);
+
+routes.post('/file', upload.single('file'), FileController.store);
 
 export default routes;
